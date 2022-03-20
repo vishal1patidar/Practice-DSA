f1f18e75fa44d1f3f8972fa8d75d58b21f718747
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-03-20 22:13:11 +0530

    Time: 141 ms (78.18%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -18,38 +18,38 @@
 // CODE WITH EXPLANATION
 
 			
-class Solution {
-public:
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n = tops.size();
+// class Solution {
+// public:
+//     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+//         int n = tops.size();
         
-        // faceA for counting the occurence in tops
-		// faceB for counting occurence of numbers in bottoms
-        // same for counting when both tops and bottoms have same occurences
-        vector<int> faceA(7), faceB(7), same(7);
+//         // faceA for counting the occurence in tops
+// 		// faceB for counting occurence of numbers in bottoms
+//         // same for counting when both tops and bottoms have same occurences
+//         vector<int> faceA(7), faceB(7), same(7);
         
-        for(int i = 0; i < n; ++i)
-        {
-            ++faceA[tops[i]];
-            ++faceB[bottoms[i]];
+//         for(int i = 0; i < n; ++i)
+//         {
+//             ++faceA[tops[i]];
+//             ++faceB[bottoms[i]];
             
-            if(tops[i] == bottoms[i])
-                ++same[tops[i]];
-        }
+//             if(tops[i] == bottoms[i])
+//                 ++same[tops[i]];
+//         }
         
-        int minRotation = INT_MAX;
+//         int minRotation = INT_MAX;
         
-		// all possibilities from 1 to 6
-        for(int i = 1; i<=6; ++i)
-        {
-            if(faceA[i] + faceB[i] - same[i] == n)
-                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
-        }
+// 		// all possibilities from 1 to 6
+//         for(int i = 1; i<=6; ++i)
+//         {
+//             if(faceA[i] + faceB[i] - same[i] == n)
+//                 minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+//         }
         
-        return minRotation == INT_MAX ? -1 : minRotation;
+//         return minRotation == INT_MAX ? -1 : minRotation;
         
-    }
-};
+//     }
+// };
 // APPROACH 2
 // AlGORITHM
 
@@ -68,41 +68,41 @@ public:
 // SPACE COMPLEXITY :- O(1)
 // CODE WITH EXPLANATION
 
-// 					// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
-// class Solution {
-// public:
+
+class Solution {
+public:
     
-//     int dominoRotations(vector<int>& tops, vector<int>& bottoms)
-//     {
-//         int minRotation = INT_MAX;
-//         for(int i = 1; i <= 6; ++i)
-//         {
-//             int swap = 0 , j;
-//             for(j = 0; j<tops.size(); ++j)
-//                 {
-//                     if(tops[j] == i)
-//                         continue;
-//                     else if(bottoms[j] == i)
-//                         ++swap;
-//                     else
-//                         break;
-//                 }
+    int dominoRotations(vector<int>& tops, vector<int>& bottoms)
+    {
+        int minRotation = INT_MAX;
+        for(int i = 1; i <= 6; ++i)
+        {
+            int swap = 0 , j;
+            for(j = 0; j<tops.size(); ++j)
+                {
+                    if(tops[j] == i)
+                        continue;
+                    else if(bottoms[j] == i)
+                        ++swap;
+                    else
+                        break;
+                }
                 
-//                 // if we reach at the end of vector && minRotation > swap
-//                 if(j == tops.size() && minRotation > swap)
-//                     minRotation = swap;
-//         }
-//         return minRotation == INT_MAX ? -1 : minRotation;
-//     }
+                // if we reach at the end of vector && minRotation > swap
+                if(j == tops.size() && minRotation > swap)
+                    minRotation = swap;
+        }
+        return minRotation == INT_MAX ? -1 : minRotation;
+    }
     
-//     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         
-//         // try to make all tops faces same
-//         int ab = dominoRotations(tops,bottoms);
+        // try to make all tops faces same
+        int ab = dominoRotations(tops,bottoms);
         
-//         // try to make all tops faces same
-//         int ba = dominoRotations(bottoms,tops);
+        // try to make all tops faces same
+        int ba = dominoRotations(bottoms,tops);
         
-//         return ab == -1 ? ba : ba == -1 ? ab : min(ab,ba);
-//     }
-// };
+        return ab == -1 ? ba : ba == -1 ? ab : min(ab,ba);
+    }
+};
