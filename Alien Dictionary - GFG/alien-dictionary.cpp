0eb7fda11d5c5bb789f9e4917b9bfb78022883df
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-06-12 12:32:46 +0530

    Added solution - LeetHub

---
+++ Alien Dictionary - GFG/alien-dictionary.cpp
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    string findOrder(string dict[], int N, int K) {
+        //code here
+        vector<vector<int>> adj(K);
+vector<int> deg(K);
+
+for (int i = 1; i < N; ++i)
+	for (int j = 0; j < dict[i - 1].size() && j < dict[i].size(); ++j)
+		if (dict[i - 1][j] != dict[i][j])
+		{
+			adj[dict[i - 1][j] - 'a'].push_back(dict[i][j] - 'a');
+			++ deg[dict[i][j] - 'a'];
+			break;
+		}
+
+string ret = "";
+queue<int> que;
+
+for (int i = 0; i < K; ++i)
+	if (deg[i] == 0)
+		que.push(i);
+		
+while (que.size())
+{
+	int u = que.front();
+	que.pop();
+	ret += u + 'a';
+	
+	for (int v : adj[u])
+		if (-- deg[v] == 0)
+			que.push(v);
+}
+
+return ret;
+    }
+};
+
+// { Driver Code Starts.
+string order;
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++) cin >> dict[i];
+        
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++) order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i]) f = false;
+
+        if(f)cout << 1;
+        else cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
