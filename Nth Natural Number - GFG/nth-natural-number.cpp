e9d11dbc09dcd381c0ef387915535dcd099b4fe7
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-06-13 18:15:52 +0530

    Added solution - LeetHub

---
+++ Nth Natural Number - GFG/nth-natural-number.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+	public:
+    	long long findNth(long long n){
+      long long x=0,pow=1;
+        while(n>0){
+          x+=(n%9)*pow;
+          n/=9;pow*=10;
+        }
+        
+        return x;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n , ans;
+		cin>>n;
+		Solution obj;
+		ans = obj.findNth(n);
+		cout<<ans<<endl;
+	}
+}
+
+  // } Driver Code Ends
