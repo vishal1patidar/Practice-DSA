63277f5c910db6b5452612d0251f72e675447003
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-06-21 09:54:34 +0530

    Added solution - LeetHub

--- Print alternate elements of an array - GFG/print-alternate-elements-of-an-array.cpp
+++ Print alternate elements of an array - GFG/print-alternate-elements-of-an-array.cpp
@@ -14,8 +14,8 @@ using namespace std;
 // n is the number of elements in array.
 void print(int ar[], int n)
 {
-      int i;
-   for( i=0;i<n;i=i+2){
+      
+   for( int i=0; i<n; i=i+2){
       cout<<ar[i]<<" ";
     // code here
    }
