df04a798ba4e4a619388de4b31adc7cc056743a8
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-08-24 09:20:58 +0530

    Time: 40 ms (57.90%), Space: 9 MB (91.33%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+	}
+};
