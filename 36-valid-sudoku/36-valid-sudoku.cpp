93ae77892f020f2550420d87809d899abd9ca687
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-08-30 10:24:13 +0530

    Time: 23 ms (89.24%), Space: 18 MB (89.14%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+    int rows[9][9] = {0};
+    int cols[9][9] = {0};
+    int blocks[3][3][9] = {0};
+    
+    for(int r=0;r<9;r++)
+    {
+        for(int c=0;c<9;c++)
+        {
+            if(board[r][c]=='.')continue;
+            int num = board[r][c]-'1';
+            if(cols[num][c]++ || rows[r][num]++ || blocks[r/3][c/3][num]++)
+                return false;
+        }
+    }
+    
+    return true;
+}
+};
