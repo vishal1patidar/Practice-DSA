2609cf27366e5c155dd019c4cf411edae4b44593
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-11 17:32:39 +0530

    Time: 7 ms (40.82%), Space: 7.7 MB (54.96%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,43 +1,45 @@
+/*
+											   o
+											 / | \
+										   /   |   \
+										 /     |     \
+									   /       |       \
+									 /         |         \
+								   /           |           \
+								 /             |             \ 
+								1              2              3
+							   / \            / \            / \
+							  2   3          3   1          2   1
+							 /     \        /     \        /     \
+							3       2      1       3      1       2
+							|       |      |       |      |       |
+						   123     132    231     213    321     312
+*/
 class Solution {
-private :
-
-void helper(int pos , int n , vector<int> &nums , vector<vector<int>> &ans)
-{
-    // If we have reached the end of nums[], we have found a Valid Permutations of nums[]
-
-    if(pos >= n)
-    {
-        ans.push_back(nums) ;
-        return ;
-    }
-
-    // Otherwise we iterate over all the other elements and try to generate Permutations by swapping nums[pos] with nums[i]
-
-    for(int i = pos ; i < n ; i++)
-    {
-        // We create one Unique Permutation by swapping nums[pos] with nums[i]
-
-        swap(nums[pos] , nums[i]) ;
-
-        // And we ask Recursion to handle rest of the task
-
-        helper(pos + 1 , n , nums , ans) ;
-
-        // But after we come back, we must backtrack and undo the changes we have done
-
-        swap(nums[pos] , nums[i]) ;
-    }
-
-    return ;
-}
-
 public:
+    void permutation(vector<int> &vec, int k, vector<int> &a, vector<int> &res, vector<vector<int>> &ans){
+        if(k==vec.size()){
+            ans.push_back(res);//pushing res vector to ans vector
+        }
+        else{
+            for(int i=0;i<vec.size();i++){
+			//checking if that element is already taken or not, if it is 0 then we will add it
+			//in resultant vector else we will move forward
+                if(a[i]==0){
+                    res[k] = vec[i];  //adding element to resultant vector
+                    a[i] = 1;  //Assign 1 such that next time it will not get counted
+                    permutation(vec, k+1, a, res, ans);  //calling function by increasing k
+                    a[i] = 0;  //backtracking(moving back to previous position)
+                }
+            }
+        }
+    }
+
     vector<vector<int>> permute(vector<int>& nums) {
-
-    vector<vector<int>> ans ;
-
-    helper(0 , nums.size() , nums , ans) ;
-
-    return ans ;
-}
+        vector<vector<int>> ans; 
+        vector<int> a(nums.size(), 0);  //for storing 0 & 1(vector for checking)
+        vector<int> res(nums.size());  //for storing vector elemets
+        permutation(nums, 0, a, res, ans);  //calling recursive function
+        return ans;  //returning answer
+    }
 };
