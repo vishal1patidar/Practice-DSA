6fe6470e4704711fa36aebd2f8b0c9e0bb0c03c0
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-14 07:10:05 +0530

    Time: 42 ms (17.20%), Space: 12.9 MB (69.20%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-	int deleteAndEarn(vector<int>& nums) {
-		vector<int> cnt(10002);
-		for(int i=0;i<nums.size();i++)
-		{
-			cnt[nums[i]]+=nums[i];
-		}
-
-		vector<int> dp(10002);
-		dp[1]=cnt[1];
-		dp[2]=max(cnt[1],cnt[2]);
-		for(int i=3;i<dp.size();i++)
-		{
-			dp[i]=max(dp[i-2]+cnt[i],dp[i-1]);
-		}
-
-		return max(dp[10000],dp[9999]);
-	}
+    int deleteAndEarn(vector<int>& nums) {
+        map<int, int> orderMap;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            ++orderMap[nums[i]];
+        }
+        
+        int slow = 0, fast = 0, lastElement = -1, tmpValue;
+        
+        for (auto it = orderMap.begin(); it != orderMap.end(); ++it) {
+            tmpValue = it->second * it->first;;
+            if (lastElement == it->first - 1) {
+                tmpValue += slow;
+            } else {
+                tmpValue += max(slow, fast);
+            }
+            
+            slow = max(slow, fast);
+            fast = tmpValue;
+            lastElement = it->first;
+        }
+        
+        return max(slow, fast);
+    }
 };
