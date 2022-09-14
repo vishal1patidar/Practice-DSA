208e9f716d9a30bd7f2a2a8da4516948fc4fb793
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-14 07:09:23 +0530

    Time: 4 ms (36.50%), Space: 7.7 MB (49.58%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    vector<int> mem;
-    int maxRob(vector<int> &nums , int i){
-        if (mem[i] != -1) return mem[i];
-        if (i >= nums.size()) return 0;
-        
-        return mem[i] = max(nums[i] + maxRob(nums , i+2) , maxRob(nums, i+1));
-    }
     int rob(vector<int>& nums) {
-        mem = vector<int>(nums.size()+3 , -1);
-        return maxRob(nums , 0);
+        
+        int in = nums[0];
+        int ex = 0;
+        int exn;
+        
+        for(int i=1;i<nums.size(); i++)
+        {
+            exn = max(in, ex);
+            
+            in = ex + nums[i];
+            ex = exn;
+        }
+        
+        return max(in, ex);
     }
 };
