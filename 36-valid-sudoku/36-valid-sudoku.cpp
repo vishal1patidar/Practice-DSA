8a105bbfd519b83ea0f89640cec5000bdad41488
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-15 18:07:03 +0530

    Time: 55 ms (17.85%), Space: 18.9 MB (55.09%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-    int rows[9][9] = {0};
-    int cols[9][9] = {0};
-    int blocks[3][3][9] = {0};
-    
-    for(int r=0;r<9;r++)
+	
+	unordered_map<char,int>mp;
+	
+    for(int i=0;i<board.size();i++)
     {
-        for(int c=0;c<9;c++)
+        for(int j=0;j<board[i].size();j++)
         {
-            if(board[r][c]=='.')continue;
-            int num = board[r][c]-'1';
-            if(cols[num][c]++ || rows[r][num]++ || blocks[r/3][c/3][num]++)
-                return false;
+            if(board[i][j]=='.') continue;
+            mp[board[i][j]]++;
+        }
+        for(auto i:mp) if(i.second>1) return false;
+        mp.clear();
+    }
+     for(int i=0;i<board.size();i++)
+    {
+        for(int j=0;j<board[i].size();j++)
+        {
+            if(board[j][i]=='.') continue;
+            mp[board[j][i]]++;
+        }
+        for(auto i:mp) if(i.second>1) return false;
+        mp.clear();
+    }
+    int limiti=3, limitj=3; int j=0, nowj=0,nowi=0;
+    while(limiti<10)
+    {
+        for(int i=nowi;i<limiti;i++)
+        {
+            for(j=nowj;j<limitj;j++)
+            {
+                if(board[i][j]=='.') continue;
+                mp[board[i][j]]++;
+            }
+        }
+        for(auto i : mp) if(i.second>1) return false;
+        mp.clear();  nowj+=3; limitj+=3;
+        if(nowj>8)
+        {
+            nowj=0; limitj=3; nowi+=3; limiti+=3;
         }
     }
-    
     return true;
-}
+	
+	
+    }
 };
