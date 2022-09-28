94f8d1d0471b5e4be2cff070d2cc348d02fa2689
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-28 08:49:22 +0530

    Time: 15 ms (79.37%), Space: 10.4 MB (67.21%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution { // 8 ms, faster than 91.61%
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the smallest number >= x
+                *it = x; // Replace that number with x
+            }
+        }
+        return sub.size();
+    }
+};
