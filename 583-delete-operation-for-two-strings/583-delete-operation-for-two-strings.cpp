6df4ee8c5fb97799a459f1b6fcb73d1059786e62
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-09-30 12:11:35 +0530

    Time: 46 ms (37.97%), Space: 12.4 MB (17.93%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-int minDistance(string word1, string word2) {
-int dp[word1.size()+1][word2.size()+1];
-for(int i = 0 ; i < word2.size()+1 ; i++){
-dp[0][i]=i;
-}
-for(int i = 0 ; i< word1.size()+1;i++){
-dp[i][0]=i;
-}
-
-    for(int i = 1 ; i < word1.size()+1 ; i++){
-        for(int j = 1 ; j < word2.size()+1 ; j++){
-            if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
-            else dp[i][j] = min(dp[i-1][j],dp[i][j-1])+1;
-        }
-    }
-    return dp[word1.size()][word2.size()];
     
-}
+    int minDistance(string word1, string word2) 
+    {
+        int m=word1.length(), n=word2.length(); 
+        
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0)); 
+        
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0) continue;
+                
+                else if(word1[i-1]==word2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return m+n-2*dp[m][n];
+    }
 };
