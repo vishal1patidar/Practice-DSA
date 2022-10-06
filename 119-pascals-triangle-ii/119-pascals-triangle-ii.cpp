04b0f50868ae5861df21607461e7defeb67ad59f
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-10-06 14:23:12 +0530

    Time: 6 ms (14.90%), Space: 6.4 MB (80.44%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-  vector<int> row(rowIndex + 1);
-  row[0] = 1;
-  if (!rowIndex) return row;
-
-  for (int r = 1; r <= rowIndex; r++) {
-	int x1 = row[0];
-	int x2 = row[1];
-	for (int i = 1; i < r; i++) {
-	  x2 = row[i];
-	  row[i] += x1;
-	  x1 = x2;
-	}
-	row[r] = 1;
-  }
-  return row;
-}
+    vector<int> getRow(int n) 
+    {
+        vector<int>v(n+1);
+        v[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+		// converting int in to long long because for n>=30 number is not in range of int
+          v[i]=(long long)v[i-1]*(n+1-i)/i;
+        }
+        return v;
+    }
 };
