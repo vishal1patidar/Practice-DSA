c4f6aede09d1eb8c990e548816a8b3fe330ab8ad
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-10-19 08:52:44 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (70.58%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,22 @@
+//! Using Reverse PreOrder Traversal (Recursive)
+class Solution {
+public:
+    void RightView(TreeNode* root, int level, vector<int>&ans)
+    {
+        if(root==NULL)
+            return;
+        
+        if(level == ans.size())
+            ans.push_back(root->val);
+        
+        RightView(root->right, level+1, ans);
+        RightView(root->left, level+1, ans);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        RightView(root, 0, ans);
+        
+        return ans;
+    }
+};
