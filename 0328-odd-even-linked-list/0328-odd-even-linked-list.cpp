5629815be406996bc72c3bfccc613e61e5378033
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-10-29 10:54:02 +0530

    Time: 13 ms (89.32%), Space: 10.5 MB (28.79%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next || !head->next->next) return head;
+        
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *even_start = head->next;
+        
+        while(odd->next && even->next){
+            odd->next = even->next; //Connect all odds
+            even->next = odd->next->next;  //Connect all evens
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_start;   //Place the first even node after the last odd node.
+        return head; 
+    }
+};
