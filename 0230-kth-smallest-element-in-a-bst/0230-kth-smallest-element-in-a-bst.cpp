a07430aaae5a7e537411a0deb396d87991de47a9
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-11-03 11:26:41 +0530

    Time: 47 ms (17.60%), Space: 24.2 MB (60.81%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,19 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> a;
-//     int kthSmallest(TreeNode* root, int k) {
-//         inorder(root);
-//         int ans = a[k-1];
-//         return ans;
-//     }
-//     void inorder(TreeNode* root){
-//         if(!root) return;
-//         inorder(root->left);
-//         a.push_back(root->val);
-//         inorder(root->right);
-//     }
-// };
-
 
 class Solution {
 public:
