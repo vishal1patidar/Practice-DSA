65c7979e641848a639dce7cc9639b23bc981db20
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-11-07 10:08:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.85%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s_num = to_string(num);
+        for (auto &c : s_num) {
+            if (c == '6') {
+                c = '9';
+                break;
+            }
+        }
+        return stoi(s_num);
+    }
+};
