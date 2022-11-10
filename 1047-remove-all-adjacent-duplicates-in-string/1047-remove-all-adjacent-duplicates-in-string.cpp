952a3e4316fd08b3e3c6a04c3910c84f23de3bac
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-11-10 09:19:52 +0530

    Time: 60 ms (48.97%), Space: 10.9 MB (66.38%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for (char c : s) {
+            if (!res.empty() && res.back() == c) {
+                res.pop_back();
+            } else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
