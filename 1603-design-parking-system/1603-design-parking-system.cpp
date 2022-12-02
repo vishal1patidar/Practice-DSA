36070121940f66a5e44d072380c75c6ac1121113
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-12-02 10:54:36 +0530

    Time: 69 ms (85.50%), Space: 33.1 MB (34.00%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,31 +1,17 @@
 class ParkingSystem {
 public:
-	int big,small,medium;
-	ParkingSystem(int big, int medium, int small) {
-		this->big=big;
-		this->medium=medium;
-		this->small=small;
-	}
+    vector<int> count;
+    ParkingSystem(int big, int medium, int small) {
+        count = {big, medium, small};
+    }
 
-	bool addCar(int carType) {
-		if(carType==1) {
-			if(big) {
-				big--;
-				return true;
-			}
-			return false;
-		}
-		else if(carType==2) {
-			if(medium) {
-				medium--;
-				return true;
-			}
-			return false;
-		}
-		if(small) {
-			small--;
-			return true;
-		}
-		return false;
-	}
+    bool addCar(int carType) {
+        return count[carType - 1]-- > 0;
+    }
 };
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
