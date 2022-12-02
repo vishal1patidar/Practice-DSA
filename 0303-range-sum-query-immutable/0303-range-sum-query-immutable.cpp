a168ca0309971503b801f5df0626c8c7261dd493
vishal patidar <79128256+vishal1patidar@users.noreply.github.com>
2022-12-02 10:54:45 +0530

    Time: 23 ms (95.82%), Space: 16.9 MB (99.70%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,14 @@
+class NumArray { 
+public:
+    vector<int>& preSum; 
+    
+    NumArray(vector<int>& nums) : preSum(nums) {
+        for (int i = 1; i < preSum.size(); ++i)
+            preSum[i] += preSum[i-1]; 
+    }
+    
+    int sumRange(int left, int right) {
+        if (left == 0) return preSum[right];
+        return preSum[right] - preSum[left-1];
+    }
+};
